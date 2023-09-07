5f8100156a4333fb5d0c632219b4c7c6dbcb8034
lakshmipathimachineni <machinenilakshmipat@cityuniversity.edu>
2023-09-07 14:52:52 -0700

    HOS10 commit

---
+++ frontend/hos09-client/src/components/login.js
@@ -0,0 +1,13 @@
+//HOS10A login.js
+import React from "react";
+
+const Login = () => {
+  return (
+    <a href="https://jubilant-telegram-x6xjprgw5gj3vgx6-5050.app.github.dev/facebook" 
+    className="btn btn-primary">
+      Login with Facebook
+    </a>
+  );
+};
+
+export default Login;
